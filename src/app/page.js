d3658ca0899dfa74983d911e6b42bd74a26cd6b4
Emmanuelle Curiant <emmanuelle.curiant@gmail.com>
2024-12-04 15:21:40 +0100

    fix layout + page.js

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <h1>c'est la page page.js</h1>
+      </main>
+      <footer>
+        <p>c'est le footer</p>
+      </footer>
+    </div>
+  );
+}
