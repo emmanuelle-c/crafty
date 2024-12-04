1d46f94cc2e98ff276d40653e7ff9709969b5074
Emmanuelle Curiant <emmanuelle.curiant@gmail.com>
2024-12-04 17:23:30 +0100

    server express encapsulated into server next

--- src/app/server.js
+++ src/app/server.js
@@ -1,36 +1,36 @@
 require("dotenv").config();
 
+const next = require('next');
 const express = require("express");
-const cors = require("cors");
-const app = express();
+const server = express();
 
-const router = express.Router();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.use(
-    cors({
-      exposedHeaders: ["Authorization"],
-      origin: [
-        process.env.CLIENT_URL,
-      ],
-      credentials: true,
-    })
-  );
-
-app.use("/api", router);
-app.get("/api", (req,res) => res.send("it's all good"))
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
 const port = process.env.APP_PORT;
 
-// Start the server and listen on the specified port
-app
-.listen(port, () => {
-    console.info(`Server is listening on port ${port}`);
-})
-.on("error", (err) => {
+app.prepare().then(() => {
+  const server = express();
+  const router = express.Router();
+
+  server.use(express.json());
+  server.use(express.urlencoded({ extended: false }));
+
+  server.use('/api', router);
+  server.get("/api", (req,res) => res.send("it's all good"))
+
+  // Toutes les autres routes sont gérées par Next.js
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, () => {
+    console.log('Serveur prêt sur http://localhost:3000');
+  })
+  .on("error", (err) => {
     console.error("Error:", err.message);
+  });
 });
 
 module.exports = app;
